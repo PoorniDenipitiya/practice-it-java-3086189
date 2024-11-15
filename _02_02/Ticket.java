bdb3da8b52b96f5d29025b72797a4314e572287c
Poorni Denipitiya <110786884+PoorniDenipitiya@users.noreply.github.com>
2024-11-15 22:13:53 +0000

    changes

---
+++ _02_02/Ticket.java
@@ -0,0 +1,9 @@
+public class Ticket{
+    //Empty Constructor
+    public Ticket(){}
+
+    //Variable declarations
+    private String destination;
+    private double price;
+    private boolean isReturn;
+}
